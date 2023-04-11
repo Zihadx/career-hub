7717cb7e803e4e8282516c71a281da85f3910c73
zihadx <blankzihad@gmail.com>
2023-04-11 20:08:32 +0600

    test

--- src/Componets/Section.jsx
+++ src/Componets/Section.jsx
@@ -10,7 +10,7 @@ const Section = () => {
   return (
     <div className="px-24">
       <div className="text-center mt-12">
-        <h1 className="text-3xl font-bold mb-4">Job Category List</h1>
+        <h1 className="text-3xl font-bold mb-4">Job Category List All Job</h1>
         <p>
           <small>
             Explore thousands of job opportunities with all the information you
