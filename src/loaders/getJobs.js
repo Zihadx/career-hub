2cfc0268d1c729ff3956c24b73bad83f94be6586
zihadx <blankzihad@gmail.com>
2023-04-12 23:43:30 +0600

    project complet

--- src/loaders/getJobs.js
+++ src/loaders/getJobs.js
@@ -3,7 +3,7 @@ import { getStoresData } from "../utils/fakeDb";
 // setAppliedJob(jobs);
 
 export const handleJobs = async () => {
-  const appliedJobsData = await fetch("job.json");
+  const appliedJobsData = await fetch("/job.json");
   const jobsData = await appliedJobsData.json();
 
   const saveJobs = getStoresData();
