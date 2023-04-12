f6b27d7deb12a370343c512c4ca819f172208e5a
zihadx <blankzihad@gmail.com>
2023-04-12 23:37:50 +0600

    fixed home page routing problem

--- src/main.jsx
+++ src/main.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
-        loader: () => fetch("job.json"),
+        loader: () => fetch("/job.json"),
       },
     ],
   },
